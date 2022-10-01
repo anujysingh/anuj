61c7745baa0a415386568fc4a2e05f829b9223fd
anujysingh <anujsinghyadav1806@gmail.com>
2022-10-01 23:06:06 +0530

    Addition of two numbers

---
+++ Addition _0f_two_no/Addition _0f_two_no/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.Write("Enter Your first number:= ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Your second number:= ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int result = num1 + num2;
+        Console.WriteLine($"\nYour result is = {result}");
+        Console.ReadLine();
+    }
+    }
+
